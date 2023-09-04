d6778d44dbf7130b324032976927f4b0d6dbf191
mbrav <mbrav@protonmail.com>
2023-09-04 12:32:12 +0300

    Add wakatime hook script for fish

---
+++ dotfiles/.config/fish/conf.d/wakatime.fish
@@ -0,0 +1,35 @@
+###
+# wakatime.fish
+#
+#　hook script to send wakatime a tick (unofficial)
+# see: https://github.com/ik11235/wakatime.fish
+###
+
+function __register_wakatime_fish_before_exec -e fish_preexec
+  if set -q FISH_WAKATIME_DISABLED
+    return 0
+  end
+
+  set -l PLUGIN_NAME "ik11235/wakatime.fish"
+  set -l PLUGIN_VERSION "0.0.3"
+
+  set -l project
+  set -l wakatime_path
+
+  if type -p wakatime 2>&1 > /dev/null
+    set wakatime_path (type -p wakatime)
+  else if type -p ~/.wakatime/wakatime-cli 2>&1 > /dev/null
+    set wakatime_path (type -p ~/.wakatime/wakatime-cli)
+  else
+    echo "wakatime command not found. Please read \"https://wakatime.com/terminal\" and install wakatime."
+    return 1
+  end
+
+  if git rev-parse --is-inside-work-tree &> /dev/null
+    set project (basename (git rev-parse --show-toplevel))
+  else
+    set project "Terminal"
+  end
+
+  $wakatime_path --write --plugin "$PLUGIN_NAME/$PLUGIN_VERSION" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) &> /dev/null&
+end

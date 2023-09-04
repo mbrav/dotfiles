691526f41eafac0f72792bcea3bfc790a236ccee
mbrav <mbrav@protonmail.com>
2023-09-04 16:51:47 +0300

    move scripts to one fodler, remove separate bash and fish scripts

--- dotfiles/.bashrc
+++ dotfiles/.bashrc
@@ -36,12 +36,11 @@ command -v pyenv >/dev/null && eval "$(pyenv virtualenv-init -)"
 [[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
 
 # Init custom scripts
-[[ -f "$HOME/.config/scripts/functions.sh" ]] && source "$HOME/.config/scripts/functions.sh"
 [[ -f "$HOME/.config/scripts/aliases" ]] && source "$HOME/.config/scripts/aliases"
 [[ -f "$HOME/.config/scripts/secrets" ]] && source "$HOME/.config/scripts/secrets"
 
 # Load exceutable scripts and add to PATH
-[[ -d "$HOME/.config/scripts/bin" ]] && PATH="$PATH:$HOME/.config/scripts/bin"
+[[ -d "$HOME/.config/scripts" ]] && PATH="$PATH:$HOME/.config/scripts"
 
 # Kubectl aliases based on shell 
 # Based on https://github.com/ahmetb/kubectl-aliases
@@ -70,4 +69,54 @@ pre_prompt_command() {
 }
 
 command -v wakatime-cli > /dev/null && PROMPT_COMMAND="pre_prompt_command; $PROMPT_COMMAND"
+
+function start_tmux() {
+    if ! command -v tmux &> /dev/null; then
+        # Check if tmux is installed
+        # if not, exit function
+        return
+    fi
+ 
+    if [[ -n "$NVIM" || "$TERM_PROGRAM" = @(vscode) ]]; then
+        # Check if terminal inside an IDE
+        IN_IDE=1
+    fi
+
+    if [[ -n "$SSH_CONNECTION" || -n "$SSH_CLIENT" || -n "$SSH_TTY" || -n "$KONSOLE_DBUS_SESSION"  ]]; then
+        # $SSH_* - Check if inside a SSH session
+        # If so, do not enter a tmux session and exit function
+
+        # $KONSOLE_DBUS_SESSION - Check if inside a Konsole session
+        # Since Konsole is assumed to not be the default terminal app
+        # Whenever a integrated terminal opens within a KDE framework app
+        # exit function
+        if [[ -z "$IN_IDE" ]]; then
+            # If inside IDE, ignore
+            return
+        fi
+        # echo "🛑 Inside SSH session, not starting tmux session"
+    fi
+
+    if [[ -n "$TMUX" || "$TERM" = "screen" ]]; then
+        # Check if already inside tmux or custom variable
+        # if so, exit function
+        return
+    fi
+
+    # Attach to tmux session on shell login if tmux is installed
+    # Set default session name to "main"
+    tmux_session_name="🐺$(whoami)"
+
+    if [[ -n "$IN_IDE" ]]; then
+        # Check if term is inside an IDE or other environments
+        folder="$(pwd)"
+        folder_name="$(basename $folder)"
+        tmux_session_name="🖥️$folder_name"
+    fi
+
+    # Attach to existing or create a new tmux session
+    tmux -2 attach -t "$tmux_session_name" || tmux -2 new-session -s "$tmux_session_name"
+}
+
+start_tmux
 
--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -35,12 +35,6 @@ if test -d ~/Applications/depot_tools
 end
 
 
-## Starship prompt
-if status --is-interactive && type -q starship
-    source (starship init fish --print-full-init | psub)
-end
-
-
 ## Advanced command-not-found hook
 if test -f /usr/share/doc/find-the-command/ftc.fish
     source /usr/share/doc/find-the-command/ftc.fish
@@ -112,15 +106,17 @@ end
 
 ## MY MODS
 
+# Set trucolor
+set -x COLORTERM truecolor
+
 # Init custom scripts
-source ~/.config/scripts/functions.fish
-source ~/.config/scripts/secrets
-source ~/.config/scripts/aliases
+source ~/.config/scripts/_secrets
+source ~/.config/scripts/_aliases
 
 # Load exceutable scripts and add to PATH
-if test -d ~/.config/scripts/bin
-    if not contains -- ~/.config/scripts/bin $PATH
-        set -p PATH ~/.config/scripts/bin
+if test -d ~/.config/scripts
+    if not contains -- ~/.config/scripts $PATH
+        set -p PATH ~/.config/scripts
     end
 end
 
@@ -148,4 +144,66 @@ end
 if type -q mcfly
     mcfly init fish | source
 end 
+
+# init starship
+function load_starship
+    if status --is-interactive && type -q starship
+        source (starship init fish --print-full-init | psub)
+    end
+end
+
+function start_tmux
+    if not type -sq tmux; and not status --is-interactive
+        # Check if tmux is installed
+        # if not, exit function
+        # echo "🛑 Tmux not installed, not starting tmux session"
+        return
+    end
+
+    if contains "$TERM_PROGRAM" vscode; or test -n "$NVIM"
+        # Check if terminal inside an IDE
+        set IN_IDE 1
+    end
+    
+    if test -n "$SSH_CONNECTION"; or test -n "$SSH_CLIENT"; or test -n "$SSH_TTY"; or test -n "$KONSOLE_DBUS_SESSION"
+
+        # $SSH_* - Check if inside a SSH session
+        # If so, do not enter a tmux session and exit function
+
+        # $KONSOLE_DBUS_SESSION - Check if inside a Konsole session
+        # Since Konsole is assumed to not be the default terminal app
+        # Whenever a integrated terminal opens within a KDE framework app
+        # exit function
+
+        if test -z "$IN_IDE"
+            # If inside IDE, ignore
+            return
+        end
+        # echo "🛑 Inside SSH session, not starting tmux session"
+    end
+
+    if test -n "$TMUX"; or test "$SHELL" = "screen"
+        # Check if already inside tmux or custom variable
+        # if so, exit function
+        return
+    end
+
+    # Attach to tmux session on shell login if tmux is installed
+    # Set default session name to a random animal icon "main"
+    # set animal_icons ("🐺" "🐯" "🦁" "🐪" "🐧" "🦩" "🦆" "🦅" "🐼" "🦏" "🦀" "🦂" "🕷️" "🦍" "🦊" "🦖" "🐊" "🐉" "🐲" "🐍" "🐋" "🐬" "🐙")
+    set tmux_session_name "🐺$(whoami)"
+
+    if test -n "$IN_IDE"
+        # Check if term is inside an IDE or other environments
+        set folder "$(pwd)"
+        set folder_name "$(basename $folder)"
+        set tmux_session_name "🖥️$folder_name"
+    end
+
+    # Attach to existing or create a new tmux session
+    tmux -2 attach -t "$tmux_session_name"; or tmux -2 new-session -s "$tmux_session_name"
+end
+
+load_starship
+start_tmux
 

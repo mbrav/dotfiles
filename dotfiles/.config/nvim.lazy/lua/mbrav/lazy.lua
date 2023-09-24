aa25536fa0030cd698c07d1a2c88d6b91176c74f
mbrav <mbrav@protonmail.com>
2023-09-24 11:31:43 +0300

    Add lang confs to LazyVim

--- dotfiles/.config/nvim.lazy/lua/mbrav/lazy.lua
+++ dotfiles/.config/nvim.lazy/lua/mbrav/lazy.lua
@@ -13,13 +13,15 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup {
   spec = {
-    -- add LazyVim and import its plugins
+    -- Add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
+    -- Import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    -- { import = "lazyvim.plugins.extras.lang.terraform" },
+    -- Import/override with your plugins
     { import = "plugins" },
   },
   defaults = {

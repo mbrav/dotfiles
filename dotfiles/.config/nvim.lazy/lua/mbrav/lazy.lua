846122d3d2f99b0aa40b1902abccd071b3a1f5fd
mbrav <mbrav@protonmail.com>
2023-10-12 20:19:10 +0300

    Nvim update

--- dotfiles/.config/nvim.lazy/lua/mbrav/lazy.lua
+++ dotfiles/.config/nvim.lazy/lua/mbrav/lazy.lua
@@ -14,11 +14,16 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup {
   spec = {
     -- Add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim",                               import = "lazyvim.plugins" },
     -- Import any extras modules here
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    -- { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- { import = "lazyvim.plugins.extras.lang.terraform" },
     -- Import/override with your plugins
--- dotfiles/.config/nvim.lazy/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim.lazy/lua/plugins/treesitter.lua
@@ -18,6 +18,7 @@ return {
         "yaml",
         "terraform",
         "hcl",
+        "c",
       },
     },
   },

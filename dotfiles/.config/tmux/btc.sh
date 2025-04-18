35b8cfe1e9c55e012000fea96e556f5f06336547
mbrav <mbrav@protonmail.com>
2025-04-18 10:10:41 +0300

    fix btc

--- dotfiles/.config/tmux/btc.sh
+++ dotfiles/.config/tmux/btc.sh
@@ -2,13 +2,14 @@
 
 # Define the temporary file to store the Bitcoin price
 txt_display="₿"
+cur_to="usd"
 tmp_file="/tmp/bitcoin_price.json"
 api_url="https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${cur_to}"
 
 # Function to fetch the Bitcoin price
 fetch_bitcoin_price() {
 	# Call the API and save the result to the temporary file
-	curl -s "${api_url}" >"${tmp_file}"
+	curl -s "${api_url}" -o "${tmp_file}"
 }
 
 # Check if the temporary file exists and if it's older than 1 hour
@@ -18,7 +19,7 @@ fi
 
 # Output the current date and the Bitcoin price
 if [[ -f "${tmp_file}" ]]; then
-	btc_price_to_usd=$(jq -r ".bitcoin.usd" "$tmp_file")
+	btc_price_to_usd=$(jq -r ".bitcoin.${cur_to}" "$tmp_file")
 	echo "${txt_display}${btc_price_to_usd}"
 else
 	echo "No BTC"

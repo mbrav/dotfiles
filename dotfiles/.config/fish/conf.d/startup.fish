c98d101f0ab1771a7d3114bf909dd3f519aa243f
mbrav <mbrav@protonmail.com>
2026-06-13 18:31:28 +0300

    fix fastfetch

--- dotfiles/.config/fish/conf.d/startup.fish
+++ dotfiles/.config/fish/conf.d/startup.fish
@@ -1,7 +1,7 @@
 # Run fastfetch/neofetch/screenfetch on interactive session start
 if status --is-interactive
     if type -q fastfetch
-        fastfetch --load-config neofetch.jsonc
+        fastfetch --config neofetch.jsonc
     else if type -q neofetch
         neofetch
     else if type -q screenfetch
